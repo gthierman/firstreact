8a9881d78613aa6dafceb070c39f36d7fd71386e
Geoff Thierman <geoff.thierman@goclio.com>
2016-06-19 13:32:43 -0700

    styled repos

--- app/components/Repos.js
+++ app/components/Repos.js
@@ -14,12 +14,12 @@ class Repos extends Component {
   render() {
     var repos = this.props.repos;
     var list = repos.map((item, index) => {
-      var desc = repos[index].description ? <Text>{repos[index].description}</Text> : <View />;
+      var desc = repos[index].description ? <Text style={styles.repoDesc}>{repos[index].description}</Text> : <View />;
       return (
         <View key={index}>
           <View style={styles.rowContainer}>
-            <Text>{repos[index].name}</Text>
-            <Text>{repos[index].stargazers_count}</Text>
+            <Text style={styles.repoTitle}>{repos[index].name}</Text>
+            <Text style={styles.stars}>{repos[index].stargazers_count}</Text>
             {desc}
           </View>
           <Separator/>
@@ -46,6 +46,15 @@ var styles = StyleSheet.create({
     paddingLeft: 16,
     paddingTop: 12,
     paddingBottom: 12
+  },
+  repoTitle: {
+    fontWeight: "bold",
+    color: "#0075bb",
+    fontSize: 16
+  },
+  repoDesc: {
+    fontSize: 12,
+    color: "#999"
   }
 });
 Repos.propType = {
